b37e484fae09aad2ee80f20192273f3eeb0ccd01
kasundm5566 <kasundinesh5566@gmail.com>
2016-06-16 11:55:53 +0530

    Fixed issue of creating a new line automatically  in 'processed.csv'.

--- src/FileReader.py
+++ src/FileReader.py
@@ -1,9 +1,9 @@
 import linecache
-
+import sys
 __author__ = 'hsenid'
 
 file = open("output.csv", "r")
-file2 = open("processed.csv", "w")
+file2 = open("processed.csv", "w+")
 
 for i in range(1, len(file.readlines()) + 1):
     line = linecache.getline("output.csv", i).split(',')
@@ -15,4 +15,8 @@ for i in range(1, len(file.readlines()) + 1):
     for j in range(0, len(line)):
         total += int(line[j])
 
-    file2.write(str(total) + " " + str(minValue) + " " + str(maxValue)+"\n")
+    s = str(i).rstrip("\n") + "," + str(total).rstrip("\n") + "," + str(maxValue).rstrip("\n") + "," + str(minValue).rstrip("\n")
+    file2.writelines(s + "\n")
+
+file.close();
+file2.close();
