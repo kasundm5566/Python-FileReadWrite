63ea9ba4ed43e913aa52fb27626eb005dbee6603
kasundm5566 <kasundinesh5566@gmail.com>
2016-06-16 12:00:05 +0530

    Variable initialization for some variables modified.

--- src/FileReader.py
+++ src/FileReader.py
@@ -8,8 +8,6 @@ file2 = open("processed.csv", "w+")
 for i in range(1, len(file.readlines()) + 1):
     line = linecache.getline("output.csv", i).split(',')
     total = 0
-    maxValue = 0;
-    minValue = 0;
     maxValue = max(line)
     minValue = min(line)
     for j in range(0, len(line)):
